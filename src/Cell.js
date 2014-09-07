47f9fb4e483fe1ba797aa9cec8f2774b14f08332
HopefulLlama <jonlaw10170@gmail.com>
2014-09-07 11:05:43 +0100

    - Added basic interaction. Can toggle alive status of cells with UI feedback

--- src/Cell.js
+++ src/Cell.js
@@ -5,5 +5,10 @@ function Cell(){
     function setAlive(alive){
         this.alive = alive;
     }
+
+    this.toggleAlive = toggleAlive;
+    function toggleAlive(){
+    	this.alive = !this.alive;
+    }
 }
 
--- src/Drawer.js
+++ src/Drawer.js
@@ -3,17 +3,15 @@
  */
  function Drawer(){
  	this.cellWidth = 15;
+ 	this.aliveFill = '#bbb';
+ 	this.deadFill = '#fff';
 
  	this.canvas;
- 	this.rectangleArray = new Array();
  	
  	this.initialise = initialise;
  	function initialise () {
 		var width = (controller.grid.cells.length*controller.drawer.cellWidth)+5;
 		var height = (controller.grid.cells[0].length*controller.drawer.cellWidth)+5;
-
-		console.log(width);
-		console.log(height);
 
  		this.canvas = oCanvas.create({
  			canvas: '#canvas',
@@ -23,31 +21,28 @@
  		});	
 		
  		for (var i = 0; i < controller.grid.cells.length; i++){
- 			var rectangleRowArray = new Array();
  			for (var j = 0; j < controller.grid.cells[0].length; j++){ 				
  				var rectangle;
- 				if((i+1)%5 === 0 || (j+1)%5 === 0 ){
- 					rectangle = controller.drawer.canvas.display.rectangle({
-						x: (i*controller.drawer.cellWidth)+2,
-						y: (j*controller.drawer.cellWidth)+2,
-						width: controller.drawer.cellWidth,
-						height: controller.drawer.cellWidth,
-						fill: "#bbb",
-						stroke: "outisde 2px rgba(0, 0, 0, 1)"
-					});
- 				} else {
-					rectangle = controller.drawer.canvas.display.rectangle({
-						x: (i*controller.drawer.cellWidth)+2,
-						y: (j*controller.drawer.cellWidth)+2,
-						width: controller.drawer.cellWidth,
-						height: controller.drawer.cellWidth,
-						stroke: "outisde 2px rgba(0, 0, 0, 1)"
-					});
-				}
-				rectangleRowArray.push(rectangle);
+				rectangle = controller.drawer.canvas.display.rectangle({
+					x: (i*controller.drawer.cellWidth)+2,
+					y: (j*controller.drawer.cellWidth)+2,
+					width: controller.drawer.cellWidth,
+					height: controller.drawer.cellWidth,
+					stroke: "outisde 2px rgba(0, 0, 0, 1)"
+				});
+				rectangle.column = i;
+ 				rectangle.row = j;
+ 				rectangle.bind("click tap", function () {
+ 					controller.grid.cells[this.column][this.row].toggleAlive();
+ 					if (controller.grid.cells[this.column][this.row].alive) {
+ 						this.fill = controller.drawer.aliveFill; 
+ 					} else {
+ 						this.fill = controller.drawer.deadFill;
+ 					}
+ 					controller.drawer.canvas.redraw();
+				});
 				controller.drawer.canvas.addChild(rectangle);
  			}
- 			controller.drawer.rectangleArray.push(rectangleRowArray);
  		}
 
  		controller.drawer.canvas.redraw();
