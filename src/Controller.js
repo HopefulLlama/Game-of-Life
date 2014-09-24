2e282b19aa14431413f14ba0272e6d610310e3fd
HopefulLlama <jonlaw10170@gmail.com>
2014-09-24 20:27:58 +0100

    - Changed play/edit to be buttons - Added play state - Added functionality to play/edit buttons - Changed Columns and Rows textboxes to load on initialisation

--- src/Controller.js
+++ src/Controller.js
@@ -1,22 +1,27 @@
 var controller;
-var test;
+$(window).load(function (){
+    controller = new Controller();
+    controller.initialise();
+});
+
 function Controller(){
     this.grid;
     this.drawer;
     this.uiHandler;
+    this.play = false;
 
     this.initialise = initialise;
     function initialise(){
     	this.grid = new Grid(10, 5);
     	this.drawer = new Drawer();
     	this.uiHandler = new UIHandler();
-    	test = this;
     	this.uiHandler.initialise();
     	this.drawer.initialise();
     }
-}
 
-$(window).load(function (){
-	controller = new Controller();
-	controller.initialise();
-});
+    this.toggleMode = toggleMode;
+    function toggleMode() {
+        this.play = !this.play;
+        this.uiHandler.toggleButtonState(this.play);
+    }
+}
--- src/UIHandler.js
+++ src/UIHandler.js
@@ -11,5 +11,37 @@ function UIHandler() {
 
 		this.htmlCanvas.width = width;
 		this.htmlCanvas.height = height;
+
+		this.bindEvents();
 	}
+
+	this.bindEvents = bindEvents;
+	function bindEvents() {
+		/*
+		 *  Play/Edit buttons
+		 */
+		$("#playButton").click(function (event) {
+			controller.toggleMode.call(controller);
+		});
+		$("#editButton").click(function (event) {
+			controller.toggleMode.call(controller);
+		});
+
+		$("#columnTextbox").val(controller.grid.columns);
+		$("#rowTextbox").val(controller.grid.rows);
+	}
+
+	this.toggleButtonState = toggleButtonState;
+	function toggleButtonState(play) {
+		var playButton = $("#playButton");
+		var editButton = $("#editButton");
+		if (play) {
+			playButton.attr("disabled", "disabled");
+			editButton.removeAttr("disabled");
+		} else {
+			playButton.removeAttr("disabled");
+			editButton.attr("disabled", "disabled");
+		}
+	}
+
 }
